9dd4670d3a1741277671ec35d089a121c52447ce
Mahnoor225 <mahnoormehboob225@gmail.com>
2025-10-12 14:39:57 -0700

    This project is an Ecommerce web application built with Next.js. Currently, I’m working on the initial setup and installation process.

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Button className=" mt-6 ml-6">Click Me</Button>
+    </div>
+  )
+}
+
+export default page
